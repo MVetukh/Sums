53b7b936d9fca54cd4eb4fe5119115e4f534c574
M.Vetukh <m.vetux@yandex.ru>
2023-03-17 23:33:33 +0300

    sums

---
+++ ExSum All/ExSum/Program.cs
@@ -0,0 +1,453 @@
+﻿using System;
+
+
+
+namespace ExSum
+
+{
+
+    public class FinitSumClass
+    {
+        public const int problemAmount = 51; 
+        private static int problem;
+        public static void Main()
+        {
+            do
+            {
+                Console.WriteLine("Ведите номер задачи");
+                //problem = int.Parse(Console.ReadLine());
+                while(!int.TryParse(Console.ReadLine(),out problem)||problem<0|| problem>problemAmount)
+                {
+                    Console.WriteLine("Неверный номер задачи");
+                }
+                Console.WriteLine("Введите количество суммируемых членов суммы");
+                int k = int.Parse(Console.ReadLine());
+                double controlValue = ControlValue(k);
+                double sum = 0;
+                int blProblem = (problem) / 11;
+                int step = 1 + (blProblem & 1);
+                int p = 1;
+                int mult = 1 - (blProblem / 2) * 2;//problem <= 21 ? 1 : -1;  
+
+                for (int n = (problem >= 44) & (problem <= 47) ? 0 : 1; n <= step * k; n += step)
+                {
+                    sum += problem >= 44 ? CurTerm(n) : CurTerm(n) * p;
+                    p *= mult;
+                }
+                Console.WriteLine("sum = {0:n0}; control value = {1:n0}", sum, controlValue);
+                Console.WriteLine("Нажмите ESC для завершение прогаммы");
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+        }
+        private static double CurTerm(int n)
+        {
+            if (problem < 44)
+            {
+                int temp1 = (problem % 11);
+                return Math.Pow(n, temp1);
+            }
+            else
+            {
+                switch (problem)
+
+                {
+                    case 44:
+                        return (1 + 2 * n);
+                    case 45:
+                        return (1 + 3 * n + 3 * n * n);
+                    case 46:
+                        return (1 + 4 * n + 6 * n * n + 4 * n * n * n);
+                    case 47:
+                        return (1 + 5 * n + 10 * n * n + 10 * n * n * n + 5 * n * n * n * n);
+                    case 48:
+                        return (2 * n - 1);
+                    case 49:
+                        return (3 * n * 3 - 3 * n + 1);
+                    case 50:
+                        return (4 * n * n * n - 6 * n * n + 4 * n - 1);
+                    case 51:
+                        return (5 * n * n * n * n - 10 * n * n * n + 10 * n * n - 5 * n + 1);
+                    default: return Double.NaN;
+                }
+            }
+        }
+        private static double ControlValue(int k)
+        {
+            int mult1 = (k & 1) * 2 - 1;
+            int mult2 = (k & 1);
+            switch (problem)
+            {
+                // первый блок суммы вида n^p 
+                case 0:
+                    return
+                        k;
+                case 1:
+                    return
+                        Math.Pow(k, 2) / 2 + k / 2.0;
+                case 2:
+                    return
+                        Math.Pow(k, 3) / 3 + Math.Pow(k, 2) / 2 + k / 6.0;
+                case 3:
+                    return
+                        Math.Pow(k, 4) / 4 + Math.Pow(k, 3) / 2 + Math.Pow(k, 2) / 4;
+                case 4:
+                    return
+                        Math.Pow(k, 5) / 5 + Math.Pow(k, 4) / 2 + Math.Pow(k, 3) / 3 - k / 42.0;
+                case 5:
+                    return
+                        Math.Pow(k, 6) / 6 + Math.Pow(k, 5) / 2 + Math.Pow(k, 4) / 12 - Math.Pow(k, 2) / 12;
+                case 6:
+                    return
+                        Math.Pow(k, 7) / 7 + Math.Pow(k, 6) / 2 + Math.Pow(k, 5) / 2 - Math.Pow(k, 3) / 6 + k / 42.0;
+                case 7:
+                    return
+                        Math.Pow(k, 8) / 8 + Math.Pow(k, 7) / 2 + 7 * Math.Pow(k, 6) / 12 - 7 * Math.Pow(k, 4) / 24 + Math.Pow(k, 2) / 12;
+
+
+
+                case 8:
+
+
+
+                    return
+
+
+
+                        Math.Pow(k, 9) / 9 + Math.Pow(k, 8) / 2 + 2 * Math.Pow(k, 7) / 3 - 7 * Math.Pow(k, 5) / 15 + 2 * Math.Pow(k, 3) / 9 - k / 30.0;
+
+
+
+                case 9:
+
+
+
+                    return
+
+
+
+                        Math.Pow(k, 10) / 10 + Math.Pow(k, 9) / 2 + 3 * Math.Pow(k, 8) / 4 - 7 * Math.Pow(k, 6) / 10 + Math.Pow(k, 4) / 2 - 3 * k * k / 20.0;
+
+
+
+                case 10:
+
+
+
+                    return
+
+
+
+                        Math.Pow(k, 11) / 11 + Math.Pow(k, 10) / 2 + 5 * Math.Pow(k, 9) / 6 - Math.Pow(k, 7) + Math.Pow(k, 5) - Math.Pow(k, 3) / 2 + 5 * k / 66.0;
+
+
+
+                //второй блок суммы ввида (2n-1)^p, есть ошибка  
+
+
+
+                case 11:
+
+
+
+                    return
+
+
+
+                        k;
+
+
+
+                case 12:
+
+
+
+                    return
+
+
+
+                        2 * Math.Pow(k, 2) / 2;
+
+
+
+                case 13:
+
+
+
+                    return
+
+
+
+                        Math.Pow(2, 2) * Math.Pow(k, 3) / 3 - k / 3.0;
+
+
+
+                case 14:
+
+
+
+                    return
+
+
+
+                        Math.Pow(2, 3) * Math.Pow(k, 4) / 4 - k * k;
+
+
+
+                case 15:
+
+
+
+                    return
+
+
+
+                        Math.Pow(2, 4) * Math.Pow(k, 5) / 5 - 8 * Math.Pow(k, 3) / 3 + 7 * k / 15.0;
+
+
+
+                case 16:
+
+
+
+                    return
+
+
+
+                        Math.Pow(2, 5) * Math.Pow(k, 6) / 6 - 20 * Math.Pow(k, 4) / 3 + 7 * k * k / 3.0;
+
+
+
+                case 17:
+
+
+
+                    return
+
+
+
+                        Math.Pow(2, 6) * Math.Pow(k, 7) / 7 - 16 * Math.Pow(k, 5) + 28 * k * k * k / 3 - 31 * k / 21.0;
+
+
+
+                case 18:
+
+
+
+                    return
+
+
+
+                        Math.Pow(2, 7) * Math.Pow(k, 8) / 8 - 112 * Math.Pow(k, 6) / 3 + 98 * Math.Pow(k, 4) / 3 - 31 * Math.Pow(k, 2) / 3;
+
+
+
+                case 19:
+
+
+
+                    return
+
+
+
+                        Math.Pow(2, 8) * Math.Pow(k, 9) / 9 - 256 * Math.Pow(k, 7) / 3 + 1568 * Math.Pow(k, 5) / 15 - 496 * k * k * k / 9.0 + 127 * k / 15;
+
+
+
+                case 20:
+
+
+
+                    return
+
+
+
+                        Math.Pow(2, 9) * Math.Pow(k, 10) / 10 - 192 * Math.Pow(k, 8) + 1568 * Math.Pow(k, 6) / 5 - 248 * Math.Pow(k, 4) + 381 * k * k / 5.0;
+
+
+
+                case 21:
+
+
+
+                    return
+
+
+
+                        Math.Pow(2, 10) * Math.Pow(k, 11) / 11 - 1280 * Math.Pow(k, 9) / 3 + 896 * Math.Pow(k, 7) - 992 * Math.Pow(k, 5) + 508 * Math.Pow(k, 3) - 2555 * k / 33.0;
+
+
+
+                //третий блок суммы ввида (-1)^n-1*n^p   
+
+
+
+                case 22:
+
+
+
+                    return
+
+
+
+                         mult1 * Math.Pow(k, 0) * mult2;
+
+
+
+                case 23:
+
+
+
+                    return
+
+
+
+                         mult1 * k / 2.0 + mult2 * 1 / 2.0;
+
+
+
+                case 24:
+
+
+
+                    return
+
+
+
+                         mult1 * (k * k / 2.0 + k / 2.0);
+
+
+
+                case 25:
+
+
+
+                    return
+
+
+
+                        mult1 * (k * k * k / 2.0 + 3 * k * k / 4.0) - mult2 * 1 / 4.0;
+
+
+
+                case 26:
+
+
+
+                    return
+
+
+
+                        mult1 * (Math.Pow(k, 4) / 2 + k * k * k - k / 2.0);
+
+
+
+                case 27:
+
+                    return
+
+                        mult1 * (Math.Pow(k, 5) / 2 + 5 * Math.Pow(k, 4) / 4 - 5 * k * k / 4.0) + mult2 * 1 / 2.0;
+
+                case 28:
+
+                    return
+
+                        mult1 * (Math.Pow(k, 6) / 2 + 3 * Math.Pow(k, 5) / 2 - 5 * Math.Pow(k, 3) / 2 + 3 * k / 2.0);
+
+                case 29:
+
+                    return
+
+                        mult1 * (Math.Pow(k, 7) / 2 + 7 * Math.Pow(k, 6) / 4 - 35 * Math.Pow(k, 4) / 8 + 21 * k * k / 4.0) - mult2 * 17 / 8.0;
+
+                case 30:
+
+                    return
+
+                        mult1 * (Math.Pow(k, 8) / 2 + 2 * Math.Pow(k, 7) - 7 * Math.Pow(k, 5) + 14 * k * k * k) - mult2 * 17 / 2.0;
+
+                case 31:
+
+                    return
+
+                        mult1 * (Math.Pow(k, 9) / 2 + 9 * Math.Pow(k, 8) / 4 - 21 * Math.Pow(k, 6) / 2 + 63 * Math.Pow(k, 4) / 2 - 153 * k * k / 4.0) + mult2 * 34 / 2 / 0;
+
+                case 32:
+
+                    return
+
+                        mult1 * (Math.Pow(k, 10) / 2 + 5 * Math.Pow(k, 9) / 2 - 15 * Math.Pow(k, 7) + 63 * Math.Pow(k, 5) - 255 * k * k * k / 2.0 + 155 * k / 2);
+
+                // 4 блок суммы вида (-1)^(n-1)*(2n-1)^p  
+
+                case 33:
+
+                    return
+
+                        mult1 * 1;
+
+                case 34:
+
+                    return
+
+                        mult1 * k;
+
+                case 35:
+
+                    return
+
+                        mult1 * 2 * k * k - mult2 * 1;
+
+                case 36:
+
+                    return
+
+                        mult1 * (2 * 2 * k * k * k - 3 * k);
+
+                case 37:
+
+                    return
+
+                        mult1 * (2 * 2 * 2 * Math.Pow(k, 4) - 12 * k * k) + mult2 * 5;
+
+                case 38:
+
+                    return
+
+                        mult1 * (Math.Pow(2, 4) * Math.Pow(k, 5) - 40 * k * k * k + 25 * k);
+
+                case 39:
+
+                    return
+                        mult1 * (Math.Pow(2, 5) * Math.Pow(k, 6) - 120 * Math.Pow(k, 4) + 150 * k * k) - mult2 * 61;
+                case 40:
+                    return
+                        mult1 * (Math.Pow(2, 6) * Math.Pow(k, 7) - 336 * Math.Pow(k, 5) + 700 * k * k * k - 427 * k);
+                case 41:
+                    return
+                        mult1 * (Math.Pow(2, 7) * Math.Pow(k, 8) - 896 * Math.Pow(k, 6) + 2800 * Math.Pow(k, 4) - 3416 * k * k) + mult2 * 1385;
+                case 42:
+                    return
+                        mult1 * (Math.Pow(2, 8) * Math.Pow(k, 9) - 2304 * Math.Pow(k, 7) + 10080 * Math.Pow(k, 5) - 20496 * k * k * k + 12465 * k);
+                case 43:
+                    return
+                        mult1 * (Math.Pow(2, 9) * Math.Pow(k, 10) - 5760 * Math.Pow(k, 8) + 33600 * Math.Pow(k, 6) - 102480 * Math.Pow(k, 4) + 124650 * k * k) - mult2 * 50521;
+                case 44:
+                    return k * k;
+                case 45:
+                    return k * k * k;
+                case 46:
+                    return k * k * k * k;
+                case 47:
+                    return k * k * k * k * k;
+                case 48:
+                    return k * k;
+                case 49:
+                    return k * k * k;
+                case 50:
+                    return k * k * k * k;
+                case 51:
+                    return k * k * k * k * k;
+                default: return double.NaN;
+            }
+        }
+    }
+}
---
+++ InfSum 2.0/InfSum 2.0/Program.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Text;//пространство имен для стринг билдера
+using System.Diagnostics;//пространство имен, в котором использутеся стопвоч
+using System.IO;//для текстврайтера
+
+
+namespace InfSum_2
+{
+    internal class Program
+    {
+        public static int problem, m;
+        public static double a, k;
+        const int limittime = 10;
+        static void Main(string[] args)
+        {
+            StringBuilder report = new StringBuilder();
+            string text;//вспомогательная строка; с помощью нового класса  в эту строку добавляется новое значение
+            do
+            {
+                Console.Write(text = "Введите номер задачи:");
+                problem = int.Parse(Console.ReadLine());
+                report.AppendLine(text + problem);
+                double error;
+                Console.Write(text = "Введите погрешность от 0 до 0.1:");
+                while (!double.TryParse(Console.ReadLine(), out error) || error < 0 || error > 0.1)
+                {
+                    Console.Write("Некорректно введена погрешность.");
+                }
+                report.AppendLine(text + error);//append оставляет введеные значение в одной строке, appendline - в следующей
+                double sum = problem < 5 ? 1.0 : 0;
+                double curcterm = 1;//начальный множитель
+                if (problem == 20)
+                {
+                    Console.Write(text = "Введите натуральный параметр m:");
+                    m = int.Parse(Console.ReadLine());
+                    report.AppendLine(text + m);
+                }
+                if (problem == 21)
+                {
+                    Console.Write(text = "Введите положительный параметр а, не превыщающий 1:");
+                    while (!double.TryParse(Console.ReadLine(), out a) || a > 1 || a < 0)
+                    {
+                        Console.WriteLine("некорректное значение параметра а, введите заново:");
+                    }
+                    report.AppendLine(text + a);
+                }
+
+                Stopwatch timer = new Stopwatch();// ввод нового класса
+                bool machineT(double k)//метод машинной точности, используем буль для проверки правильности значения
+                {
+                    curcterm = problem < 7 ? curcterm * Factory(k) : CurTerm(k);
+                    bool time = timer.Elapsed.TotalSeconds < limittime;// пока время не достигнет лимита, цикл будет работать
+                    return error < 1e-16 ? sum + curcterm != sum : Math.Abs(curcterm / sum) > error&&time;//если погрешность меньше 10^-16, то
+                    //вычисления проводятся с машинной точностью(сумма не меняется)
+
+                }
+               // double k;
+                timer.Start();
+                for (k = problem == 21 ? 0 : 1; machineT(k); k++)
+                {
+                    sum += curcterm;
+                }
+                timer.Stop();
+
+                double err = Math.Abs(sum - ControlValue());
+
+                Console.WriteLine($"Время выполнения = {timer.Elapsed.Seconds} c");
+                Console.WriteLine(text = $"Сумма равна {sum:g7}; Контрольное значение = {ControlValue()}");
+                report.AppendLine(text);
+                Console.WriteLine(text = $"Текущее значение k={k}");
+                report.AppendLine(text);
+                Console.WriteLine(text = $"Разница между суммой и контрольным значением в итоге составляет {err:g9}");
+                report.AppendLine(text);
+                Console.WriteLine("press escape to continue");
+
+
+
+                //эта штука спрашивает номер задачи: если 21, то k=0
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+            using (TextWriter wr = File.CreateText("InfSum.txt"))
+                wr.Write(report);//с помощью класса TwxtWriter создаем новый текстовый файл, где выводятся все накопленные значения
+        }
+        public static double Factory(double k)
+        {
+            switch (problem)
+            {
+                case 1:
+                    return 1.0 / k;
+                case 2:
+                    return -1.0 / k;
+                case 3:
+                    return 1.0 / (2 * k * (2 * k - 1));
+                case 4:
+                    return -1.0 / (2 * k * (2 * k - 1));
+                case 5:
+                    return 1.0 / (2 * k - 1) / k;
+                case 6:
+                    return Math.Pow(-1, k - 1) / (2 * k - 1) / k;
+
+                default: return Double.NaN;
+            }
+        }
+        public static double CurTerm(double k)
+        {
+            switch (problem)
+            {
+                case 7:
+                    return 1.0 / k / k;
+                case 8:
+                    return 1.0 / (2 * k - 1) / (2 * k - 1);
+                case 9:
+                    return Math.Pow(-1, k + 1) / (k * k);
+                case 10:
+                    return 1 / Math.Pow(k, 4);
+                case 11:
+                    return 1 / Math.Pow(k, 6);
+                case 12:
+                    return 1 / Math.Pow(k, 8);
+                case 13:
+                    return Math.Pow(-1, k - 1) / k;
+                case 14:
+                    return Math.Pow(-1, k - 1) / (2 * k - 1);
+                case 15:
+                    return Math.Pow(-1, k - 1) / Math.Pow(2 * k - 1, 3);
+                case 16:
+                    return 1.0 / k / (k + 1);
+                case 17:
+                    return 1.0 / (2 * k - 1) / (2 * k + 1);
+                case 18:
+                    return 1.0 / (k - 1) / (k + 1);
+                case 19:
+                    return 1.0 / k / (k + 1) / (k + 2);
+                case 20:
+                    // Здесь я хочу ввести локальные множители
+                    double m = 1;
+                    int FactM;
+                    for (FactM = 1; FactM <= m; FactM++)
+                    {
+                        m *= (k + FactM);
+                    }
+                    return 1 / m;
+                case 21:
+                    return Math.Pow(a, k);
+
+                default: return double.NaN;
+            }
+        }
+        public static double ControlValue()
+        {
+            switch (problem)
+            {
+                case 1:
+                    return Math.E;
+                case 2:
+                    return 1 / Math.E;
+                case 3:
+                    return Math.Cosh(1);
+                case 4:
+                    return Math.Cos(1);
+                case 5:
+                    return Math.Sinh(1);
+                case 6:
+                    return Math.Sin(1);
+                case 7:
+                    return Math.Pow(Math.PI, 2) / 6;
+                case 8:
+                    return Math.Pow(Math.PI, 2) / 8;
+                case 9:
+                    return Math.Pow(Math.PI, 2) / 12;
+                case 10:
+                    return Math.Pow(Math.PI, 4) / 90;
+                case 11:
+                    return Math.Pow(Math.PI, 6) / 945;
+                case 12:
+                    return Math.Pow(Math.PI, 8) / 9450;
+                case 13:
+                    return Math.Log(2);
+                case 14:
+                    return Math.PI / 4;
+                case 15:
+                    return Math.Pow(Math.PI, 3) / 32;
+                case 16:
+                    return 1;
+                case 17:
+                    return 1 / 2.0;
+                case 18:
+                    return 3 / 4.0;
+                case 19:
+                    return 1 / 4.0;
+                case 20:
+                    double Factm = 1;
+                    double n;
+                    for (n = 1; n <= Factm; n++)
+                    {
+                        Factm *= n;
+                    }
+                    return 1.0 / (m * Factm);
+                case 21:
+                    return 1 / (1 - a);
+                default:
+                    return double.NaN;
+            }
+        }
+    }
+}
